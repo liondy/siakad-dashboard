7f8f37099061bf822f7006d38558c882e60fa054
Michael Liondy <mike@liondy.com>
2023-10-03 00:01:37 +0700

    feat: init page auth and home slicing layout from stisla dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages/blank-page', ['type_menu' => '']);
-});
+    return view('pages.app.dashboard-siakad', ['type_menu' => 'dashboard']);
+})->name('dashboard');
+
+Route::get('/login', function () {
+    return view('pages.auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('pages.auth.register');
+})->name('register');
+
+Route::get('/forgot-password', function () {
+    return view('pages.auth.forgot-password');
+})->name('forgot');
+
+Route::get('/reset-password', function () {
+    return view('pages.auth.reset-password');
+})->name('reset-password');
+
